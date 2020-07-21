073d34338835ab89bf9e44700477561f457407e1
MAKCOH <dramdmg6@gmail.com>
2020-07-22 00:06:50 +0700

    PhoneBookJQuery: частично реализовал удаление и подгрузил font-awesome

--- PhoneBookJQuery/js/script.js
+++ PhoneBookJQuery/js/script.js
@@ -13,7 +13,7 @@ $(function () {
         var phoneValue = personPhone.val();
 
         var newTableRow = $("<tr>");
-        newTableRow.html("<th class='index_number' scope='row'></th><td class='name_value'></td><td class='surname_value'></td><td class='phone_value'></td><td>x</td>");
+        newTableRow.html("<th class='index_number' scope='row'></th><td class='name_value'></td><td class='surname_value'></td><td class='phone_value'></td><td><button class='btn del_button'><i class='fa fa-times' aria-hidden='true'></i></button></td>");
 
         newTableRow.find(".name_value").text(nameValue);
         newTableRow.find(".surname_value").text(surnameValue);
@@ -21,14 +21,25 @@ $(function () {
 
         table.append(newTableRow);
 
+        enumerateLines();
+
+        personName.val("");
+        personSurname.val("");
+        personPhone.val("");
+
+        $(".del_button").click(function () {
+            newTableRow.remove();
+
+            enumerateLines();
+        });
+
+    });
+
+    function enumerateLines() {
         $("#subscribers_table > tbody > tr").each(function (i) {
             var number = i + 1;
 
             $(this).find('.index_number').text(number);
         });
-
-        personName.val("");
-        personSurname.val("");
-        personPhone.val("");
-    });
+    }
 });
