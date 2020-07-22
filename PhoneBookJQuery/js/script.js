1acf24f251dd87f9939d0ec00f13f4a3b7ffe024
MAKCOH <dramdmg6@gmail.com>
2020-07-23 01:41:28 +0700

    PhoneBookJQuery: реализовал основную логику, немного подправил верстку

--- PhoneBookJQuery/js/script.js
+++ PhoneBookJQuery/js/script.js
@@ -12,6 +12,8 @@ $(function () {
         var surnameValue = personSurname.val();
         var phoneValue = personPhone.val();
 
+        //if()
+
         var newTableRow = $("<tr>");
         newTableRow.html("<th class='index_number' scope='row'></th><td class='name_value'></td><td class='surname_value'></td><td class='phone_value'></td><td><button class='btn del_button'><i class='fa fa-times' aria-hidden='true'></i></button></td>");
 
@@ -21,25 +23,29 @@ $(function () {
 
         table.append(newTableRow);
 
-        enumerateLines();
+        enumerateTableRows();
 
         personName.val("");
         personSurname.val("");
         personPhone.val("");
 
         $(".del_button").click(function () {
-            newTableRow.remove();
+            $(this).closest('tr').remove();
 
-            enumerateLines();
+            enumerateTableRows();
         });
 
     });
 
-    function enumerateLines() {
+    function enumerateTableRows() {
         $("#subscribers_table > tbody > tr").each(function (i) {
             var number = i + 1;
 
             $(this).find('.index_number').text(number);
         });
+    }
+
+    function isTextFieldEmpty(text) {
+        return text === "";
     }
 });
