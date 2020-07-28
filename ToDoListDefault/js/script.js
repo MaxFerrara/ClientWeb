b9118d297ee61d173075cc79fd9b5303bbdad2f1
MAKCOH <dramdmg6@gmail.com>
2020-07-29 01:51:34 +0700

    ToDoListDefault: исправил все кроме кнопки enter

--- ToDoListDefault/js/script.js
+++ ToDoListDefault/js/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
-    var addToDoItemButton = document.getElementById("add_item_button");
+    var addToDoItemButton = document.getElementById("add_item_button") || document.getEle;
     var toDoItemInput = document.getElementById("new_todo_item");
     var toDoList = document.getElementById("todo_list");
 
@@ -10,15 +10,31 @@ document.addEventListener("DOMContentLoaded", function () {
         var newToDoItem = document.createElement("li");
 
         if (textItem === "") {
-            alert("input field is clear");
+            alert("input field is empty");
         } else {
             switchToViewMode();
             toDoList.appendChild(newToDoItem);
             toDoItemInput.value = "";
         }
 
+      /*  addEventListener("keydown", function (event) {
+             if(event.keyCode === 13) {
+                 if (textItem === "") {
+                     alert("input field is empty");
+                 } else {
+                     switchToViewMode();
+                     toDoList.appendChild(newToDoItem);
+                     toDoItemInput.value = "";
+                 }
+            }
+        }) */
+
         function switchToViewMode() {
-            newToDoItem.innerHTML = "<span class ='item_text'></span><button class ='edit_button' type ='button'>edit</button><button class ='delete_button' type ='button'>delete</button>";
+            newToDoItem.innerHTML = "<div class='li_div'>" +
+                "<div class='first_flex_child'><span class='item_text'></span></div>" +
+                "<div><button class='edit_button' type='button'>edit</button>" +
+                "<button class='delete_button' type='button'>delete</button></div>" +
+                "</div>";
             newToDoItem.querySelector(".item_text").textContent = textItem;
 
             newToDoItem.querySelector(".delete_button").addEventListener("click", function () {
@@ -26,15 +42,22 @@ document.addEventListener("DOMContentLoaded", function () {
             });
 
             newToDoItem.querySelector(".edit_button").addEventListener("click", function () {
-                newToDoItem.innerHTML = "<input class ='new_text' type ='text'><button class ='save_button' type ='button'>save</button><button class ='cancel_button' type ='button'>cancel</button>";
-                newToDoItem.querySelector(".new_text").value = textItem;
+                newToDoItem.innerHTML = "<div class='edit_div'>" +
+                    "<div><input class='new_text' type ='text'></div>" +
+                    "<div><button class='save_button' type='button'>save</button>" +
+                    "<button class='cancel_button' type='button'>cancel</button></div>" +
+                    "</div>";
+
+                var newTextSelector = newToDoItem.querySelector(".new_text");
+
+                newTextSelector.value = textItem;
 
                 newToDoItem.querySelector(".save_button").addEventListener("click", function () {
-                    if (newToDoItem.querySelector(".new_text").value === "") {
+                    if (newTextSelector.value === "") {
                         alert("input field is clear");
-                        newToDoItem.querySelector(".new_text").value = textItem;
+                        newTextSelector.value = textItem;
                     } else {
-                        textItem = newToDoItem.querySelector(".new_text").value;
+                        textItem = newTextSelector.value;
                         switchToViewMode();
                     }
                 });
