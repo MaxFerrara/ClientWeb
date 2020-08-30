3cc0729c09c6ce5f5ae0535965dc207dd717d6c6
MAKCOH <dramdmg6@gmail.com>
2020-08-31 02:04:51 +0700

    PhoneBookVue: обновил структуру проекта, добавил разметку

---
+++ PhoneBookVue/js/script.js
@@ -0,0 +1,22 @@
+Vue.component("book-item", {
+    data: function () {
+
+    },
+    props: {
+
+    },
+    methods: {
+
+    },
+    template: "#contact-template"
+});
+
+new Vue({
+    el: "#my-phone-book",
+    data: {
+
+    },
+    methods: {
+
+    }
+});
